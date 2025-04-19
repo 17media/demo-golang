c0314df75ddd26c776dcc1c7b0c23e480beab2f9
Fionn Kuo <fionnyk_kuo@17.media>
2025-04-19 23:29:37 +0800

    test

---
+++ test2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// Function with high cyclomatic complexity (4)
+func complexFunction(a, b, c int) int {
+    if a > 10 {
+        if b > 5 {
+            return a + b
+        } else {
+            return a - b
+        }
+    } else {
+        if c < 5 {
+            return b * c
+        } else {
+            return b / c
+        }
+    }
+}
+
+func main() {
+    fmt.Println(complexFunction(12, 7, 3))  // Should print 19
+    fmt.Println(complexFunction(8, 2, 6))   // Should print 12
+}
